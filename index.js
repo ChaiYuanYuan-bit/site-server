5b18997382f4437db8753e60ccf8624aff96eaa1
Coding Boy <chaiyuanyuan432@gmail.com>
2023-04-11 10:18:12 +0800

    use nodemon fix json-server hot load

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname,'./public/database/db.json'),{'watch':path.join(__dirname,'./public/database/db.json')});
+const router = jsonServer.router(path.join(__dirname,'./public/database/db.json'));
 const middlewares = jsonServer.defaults({
     'static':'./public/source',
     'logger':true,
     "bodyParser":true,
 });
-//引入axios
-const axios  = require('axios');
 //引入cors跨域
 const cors = require('cors');
 //引入自定义路由
@@ -20,7 +18,6 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 //自定义路由中间件
 server.use(user_router);
-
 //json-server路由
 server.use(router);
 
